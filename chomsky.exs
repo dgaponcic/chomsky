f5b1e3d996a869c50ddd1e89053952f3748827a9
diana <gaponcic.d@gmail.com>
2020-04-18 03:38:05 +0300

    add inaccessible removal

--- chomsky.exs
+++ chomsky.exs
@@ -199,6 +199,72 @@ defmodule Unproductive_Removal do
   end
 end
 
+defmodule Inaccessible_removal do
+  def get_new_states(transitions, acc) do
+    transitions
+    |>  Enum.reduce([], fn transition, acc ->
+          String.graphemes(transition)
+          |> Enum.concat(acc)
+          |> Enum.uniq
+        end)
+    |> Enum.concat(acc)
+    |> Enum.uniq
+  end
+
+  def is_terminal(transition) do
+    String.length(transition) == 1 and transition == String.downcase(transition)
+  end
+
+  def is_non_terminal(transition) do
+    String.length(transition) == 1 and transition == String.upcase(transition)
+  end
+
+  def get_keys(grammar) do
+    grammar
+    |> Map.keys()
+    |> Enum.map(fn key -> Atom.to_string(key) end)
+  end
+
+  def get_all_states(grammar) do
+    Enum.reduce(grammar, get_keys(grammar), fn {_, transitions}, acc -> 
+      transitions
+      |> get_new_states(acc)
+    end)
+  end
+
+
+  def find_all_accessible_states(_, accessible, true) do
+    accessible
+  end
+
+  def find_all_accessible_states(grammar, accessible, false) do
+    new_accessible = accessible
+    |>  Enum.reduce([], fn state, acc -> 
+          if is_non_terminal(state) do 
+            Map.get(grammar, String.to_atom(state)) |> get_new_states(acc) 
+          else 
+            [state | acc]
+          end
+        end)
+    
+    find_all_accessible_states(grammar, new_accessible, new_accessible == accessible)
+  end
+
+  def filter_inaccessible(grammar, inaccessible) do
+    grammar
+    |>  Enum.reduce(%{}, fn {state, transitions}, acc -> 
+          if Enum.member?(inaccessible, Atom.to_string(state)) do acc else Map.put(acc, state, transitions) end
+        end)
+  end
+
+  def get_grammar_without_inaccessible(grammar) do
+    accessible = find_all_accessible_states(grammar, ["S"], false)
+    all_states = get_all_states(grammar)
+    inaccessible = Enum.filter(all_states, fn state -> !Enum.member?(accessible, state) end)
+    if length(inaccessible) > 0 do filter_inaccessible(grammar, inaccessible) else grammar end
+  end
+end
+
 defmodule Chomsky do
   def to_chomsky(grammar) do
     grammar
@@ -208,8 +274,10 @@ defmodule Chomsky do
     |> IO.inspect
     |> Unproductive_Removal.get_grammar_without_unproductive()
     |> IO.inspect
+    |> Inaccessible_removal.get_grammar_without_inaccessible()
+    |> IO.inspect
   end
 end
 
-%{:S => ["bA", "BC"], :A => ["a", "aS"], :B => ["A", "bS", "aAa"], :C => ["", "AB", "bAaAbE"], :D => ["AB"]}
+%{:S => ["bA", "BC"], :A => ["a", "aS", "bAaAb"], :B => ["A", "bS", "aAa"], :C => ["", "AB"], :D => ["AB"]}
 |> Chomsky.to_chomsky()
